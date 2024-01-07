aac4cbe11eb9965d9f7ff84fe54b2b4092b78628
saurabh7Goku <saurabhgk7@gmail.com>
2024-01-08 01:54:18 +0530

    created prediction pipeline

--- src/pipeline/prediction.py
+++ src/pipeline/prediction.py
@@ -1,25 +1,20 @@
-import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 import os
-
-
+import numpy as np
+import tensorflow as tf
 
 class PredictionPipeline:
     def __init__(self,filename):
         self.filename =filename
-
-
     
     def predict(self):
         ## load model
         
-        # model = load_model(os.path.join("artifacts","training", "model.h5"))
-        model = load_model(os.path.join("model", "model.h5"))
+        # model = tf.keras.models.load_model(os.path.join("artifacts","training", "model.h5"))
+        model = tf.keras.models.load_model(os.path.join("model", "model.h5"))
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
-        test_image = image.img_to_array(test_image)
+        test_image = tf.keras.preprocessing.image.load_img(imagename, target_size = (224,224))
+        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
@@ -30,3 +25,4 @@ class PredictionPipeline:
         else:
             prediction = 'Adenocarcinoma Cancer'
             return [{ "image" : prediction}]
+
