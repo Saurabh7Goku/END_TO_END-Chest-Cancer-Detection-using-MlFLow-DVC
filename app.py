6a26f88b3f164f755b58222af9792555e08d59a1
saurabh7Goku <saurabhgk7@gmail.com>
2024-01-09 02:19:18 +0530

    updated the web page

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080) #for AWS
+    app.run(host='0.0.0.0', port=8080, debug=True) #for AWS
